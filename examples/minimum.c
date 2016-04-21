e59b8448a72f2352908cf70d5148d6c86f9c77fe
Jonathan Haws <jhaws@sdl.usu.edu>
2016-04-20 21:25:10 -0600

    Adding support for GPGSV

--- examples/minimum.c
+++ examples/minimum.c
@@ -18,7 +18,8 @@ main(void)
 	//char sentence[] = "$GPGLL,4916.45,N,12311.12,W,225444,A,*1D\r\n";
 	//char sentence[] = "$GPGSA,A,1,,,,,,,,,,,,,99.99,99.99,99.99*30\r\n";
 	//char sentence[] = "$GPTXT,01,03,02,u-blox ag - www.u-blox.com*50\r\n";
-	char sentence[] = "$GPVTG,054.7,T,034.4,M,005.5,N,010.2,K\r\n";
+	//char sentence[] = "$GPVTG,054.7,T,034.4,M,005.5,N,010.2,K\r\n";
+	char sentence[] = "$GPGSV,3,1,11,03,03,111,00,04,15,270,00,06,01,010,00,13,06,292,00*74\r\n";
 
 	printf("Parsing NMEA sentence: %s", sentence);
 
@@ -42,37 +43,6 @@ main(void)
 		printf("  Degrees: %d\n", gpgll->latitude.degrees);
 		printf("  Minutes: %f\n", gpgll->latitude.minutes);
 		printf("  Cardinal: %c\n", (char) gpgll->latitude.cardinal);
-	}
-
-	if (NMEA_GPGSA == data->type)
-	{
-	  nmea_gpgsa_s *gpgsa = (nmea_gpgsa_s *) data;
-
-	  printf("GPGSA Sentence:\n");
-	  printf("\tMode: %c\n", gpgsa->mode);
-	  printf("\tFix:  %d\n", gpgsa->fixtype);
-	  printf("\tPDOP: %.2lf\n", gpgsa->pdop);
-	  printf("\tHDOP: %.2lf\n", gpgsa->hdop);
-	  printf("\tVDOP: %.2lf\n", gpgsa->vdop);
-	}
-
-	if (NMEA_GPVTG == data->type)
-	{
-	  nmea_gpvtg_s *gpvtg = (nmea_gpvtg_s *) data;
-
-	  printf("GPVTG Sentence:\n");
-	  printf("\tTrack [deg]:   %.2lf\n", gpvtg->track_deg);
-	  printf("\tSpeed [kmph]:  %.2lf\n", gpvtg->gndspd_kmph);
-	  printf("\tSpeed [knots]: %.2lf\n", gpvtg->gndspd_knots);
-	}
-
-	if (NMEA_GPTXT == data->type)
-	{
-    nmea_gptxt_s *gptxt = (nmea_gptxt_s *) data;
-
-    printf("GPTXT Sentence:\n");
-	  printf("\tID: %d %d %d\n", gptxt->id_00, gptxt->id_01, gptxt->id_02);
-	  printf("\t%s\n", gptxt->text);
 	}
 
 	if (NMEA_GPGSA == data->type)
